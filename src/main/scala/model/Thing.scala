dbdad37e65b58cb8ead8e6a832b3dcfc5141dce7
Jan Tkacik <tkacijan@fit.cvut.cz>
2015-10-31 15:28:35 +0100

    hovno commit

--- src/main/scala/model/Thing.scala
+++ src/main/scala/model/Thing.scala
@@ -3,8 +3,8 @@ package model
 /**
  * Representation of a thing to be loaded into a bag
  *
- * @param weight Weight of the thing
- * @param value Value of the thing
+ * @param weight Weight of the hovno
+ * @param value Value of the hovno
  */
 case class Thing (weight: Integer, value: Integer) {
 
